5275ad92fc16d5bcd2835b8c965aecb06dca721e
David Vorona <davidavorona@gmail.com>
2022-10-11 15:39:13 -0700

    Add commands file to tracked files

---
+++ config/commands.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        name: "ping",
+        description: "Replies with pong!"
+    },
+    {
+        name: "gex",
+        description: "Do what gex does best!"
+    }
+];
